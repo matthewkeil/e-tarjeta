396e4de9e2223a56030abec5f056cd6f1ff15a97
Jack Fischer <jrfisch95@gmail.com>
2019-03-20 12:15:17 -0400

    Added client, provider, and facility models

---
+++ server/src/models/Client.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET || "Terrible_Jwt_Secret";
+
+const Schema = mongoose.Schema;
+
+const clientSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  /**
+   * Personal Data will go here.
+   * Registration will occur before the database is updated with personal info.
+   * We do not want to require the client to finish answering
+   * all personal questions before their account is created.
+   */
+  token: String,
+  tokens: [String]
+});
+
+clientSchema.pre("save", async function(next) {
+  try {
+    const user = this;
+
+    if (!user.isModified("password")) return next();
+
+    const hash = await bcrypt.hash(user.password, 10);
+
+    user.password = hash;
+
+    next();
+    
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+clientSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+clientSchema.methods.hasValidToken = function() {
+  return !!this.token 
+    ? jwt.verify(this.token, JWT_SECRET)
+    : false;
+}
+
+clientSchema.methods.updateToken = async function() {
+    if (this.token) this.tokens.push(this.token);
+   
+    this.token = await jwt.sign({_id: this._id}, JWT_SECRET);
+}
+
+const Client =  mongoose.model("Client", clientSchema);
+
+module.exports = Client;
---
+++ server/src/models/Provider.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET || "Terrible_Jwt_Secret";
+
+const Schema = mongoose.Schema;
+
+const providerSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  license: {
+    type: String,
+    required: true
+  },
+  token: String,
+  tokens: [String]
+});
+
+providerSchema.pre("save", async function(next) {
+  try {
+    const user = this;
+
+    if (!user.isModified("password")) return next();
+
+    const hash = await bcrypt.hash(user.password, 10);
+
+    user.password = hash;
+
+    next();
+    
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+providerSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+providerSchema.methods.hasValidToken = function() {
+  return !!this.token 
+    ? jwt.verify(this.token, JWT_SECRET)
+    : false;
+}
+
+providerSchema.methods.updateToken = async function() {
+    if (this.token) this.tokens.push(this.token);
+   
+    this.token = await jwt.sign({_id: this._id}, JWT_SECRET);
+}
+
+const Provider =  mongoose.model("Provider", providerSchema);
+
+module.exports = Provider;
